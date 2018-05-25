492545cb0ac4ffb1676cb49ca43a41435f873988
smallsun <yanglingz610@sina.com>
2018-05-25 18:56:27 +0800

    match path values

---
+++ SimpleRouter.js
@@ -0,0 +1,39 @@
+function Router (path) {
+	let Router;
+    let reg1 = /(\/:\w+)/g;
+	let reg2 = /(\/\w+)/g;
+	switch (path.match(reg2).length) {
+ 	case 1:
+    	Router = function () {
+            return {
+                pattern: '/:id',
+                params: {
+                id: path.match(reg2)[0].substring(1)
+            }
+            }
+        }
+    break;
+    case 2:
+     	Router = function () {
+            return {
+                pattern: '/:group/:name',
+                params : {
+                    group: path.match(reg2)[0].substring(1),
+                    name: path.match(reg2)[1].substring(1)
+                }
+            }
+     	}
+    break;
+    default:
+    	Router = function () {
+            return {
+                pattern:'error',
+                params : null
+            }
+    	}
+    break;
+  }
+  return Router;
+} 
+
+export default Router;
